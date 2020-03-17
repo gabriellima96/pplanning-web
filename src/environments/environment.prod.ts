b25626e8c1904a857d49497f8b2468bce2ee7345
Gabriel Lima <gabriellima.silva96@gmail.com>
2020-03-17 01:10:07 -0300

    configuração para produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBCrKpvmAmKSwbX6Wohlxpktg-vfT41jFs',
+    authDomain: 'pplanning-8eff0.firebaseapp.com',
+    databaseURL: 'https://pplanning-8eff0.firebaseio.com',
+    projectId: 'pplanning-8eff0',
+    storageBucket: 'pplanning-8eff0.appspot.com',
+    messagingSenderId: '880533166165',
+    appId: '1:880533166165:web:fd4ce77c4cc5c245349cdc'
+  }
 };
